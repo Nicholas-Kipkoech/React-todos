ec02f911fe51bbcd5626b2aee8856435243bd6a8
Nicholas-Kipkoech <nickey968@gmail.com>
2022-08-22 12:50:29 +0300

    chore:todo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import { useState } from "react";
+import { ListGroup } from "react-bootstrap";
 import "./styles.css";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
-  const addTodo = () => {
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (input == "") {
+      return;
+    }
     const todos = {
       id: Math.floor(Math.random() * 1000),
-      value: input
+      value: input,
     };
     setTodos((oldList) => [...oldList, todos]);
-    setInput("");
+    setInput(" ");
   };
-
+  const deleteTodo = () => {};
   return (
     <div className="App">
-      <ul>
-        {todos.map((item) => {
-          return <li key={item.id}>{item.value}</li>;
-        })}
-      </ul>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            {todos.map((item) => {
+              return (
+                <ListGroup>
+                  <ListGroup.Item>{item.value}</ListGroup.Item>
+                </ListGroup>
+              );
+            })}
 
-      <input onChange={(e) => setInput(e.target.value)} value={input} />
-      <button onClick={addTodo}>Add todo</button>
+            <input onChange={(e) => setInput(e.target.value)} value={input} />
+            <button className="btn btn-primary" onClick={addTodo}>
+              Add todo
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
